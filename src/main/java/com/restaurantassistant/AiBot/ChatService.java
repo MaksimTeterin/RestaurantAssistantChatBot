0804c4f1aad89ee654334f9a4eda59de46873c96
Maksim Teterin <maksim.padriku@gmail.com>
2025-11-21 00:13:33 +0200

    Change ToolService to add tools attempt 2

--- src/main/java/com/restaurantassistant/AiBot/ChatService.java
+++ src/main/java/com/restaurantassistant/AiBot/ChatService.java
@@ -4,11 +4,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ChatService {
@@ -29,16 +33,19 @@ public class ChatService {
                         OpenAiChatOptions.builder()
                                 .model("gpt-4o")
                                 .temperature(0.0)
+                                .toolChoice(OpenAiChatOptions.DEFAULT_TOOL_EXECUTION_ENABLED)
                                 .user("Restaurant assistant")
                                 .build()
                 ));
         return response.getResult().getOutput().getText();
     }
 
-    @Tool(description = "Get the current date and time in the user's timezone")
-    String getCurrentDateTime() {
-        return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
-    }
+        @Tool(description = "Get the current date and time in the user's timezone")
+        String getCurrentDateTime() {
+            return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
+        }
+
+
 
 
 }
